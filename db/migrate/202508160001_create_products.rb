32bef417498cd89bab284c820a670a8b9c734559
gleebuild <gleebuild@gmail.com>
2025-08-16 05:21:22 +0000

    Update 202508160001_create_products.rb

--- db/migrate/202508160001_create_products.rb
+++ db/migrate/202508160001_create_products.rb
@@ -1,11 +1,11 @@
-class CreateProducts < ActiveRecord::Migration[7.0]
+class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :shop_products do |t|
       t.string :title
       t.text :description
-      t.jsonb :images, default: []
+      t.jsonb :images, default: [], null: false
       t.string :video_url
-      t.jsonb :specs, default: []
+      t.jsonb :specs, default: [], null: false
       t.integer :created_by
       t.integer :updated_by
       t.timestamps
